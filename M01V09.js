3bc1b14b3c5587d2d18105260991ab0d2d58328c
Mokarama <mokaramaaktershanta@gmail.com>
2025-11-02 23:40:07 +0600

    added Array.from method

--- M01V09.js
+++ M01V09.js
@@ -1,4 +1,35 @@
+
+/*
 const set = new Set();
 
-set.add("mango");
+set.add("Mango");
+set.add("Banana");
+set.add("Apple");
+set.add("apple");
 console.log(set)
+console.log(set.size);
+
+*/
+
+/****************************************** */
+const shanta={userName: "Shanta"};
+const sabbir={userName: "Mizan"};
+const tanvir={userName: "Tanvir"};
+
+const set=new Set();
+
+set.add(shanta);
+set.add(sabbir);
+set.add(tanvir);
+set.add(tanvir);
+
+console.log(set);
+
+
+/**********duplicate item REMOVE */
+
+const arr =["apple", "mango", "banana"];
+
+const set2 =new Set(arr);
+set2.push("tomato");
+console.log(set2);
---
+++ set.js
@@ -0,0 +1,13 @@
+const arr=["mango", "banana"];
+
+const set = new Set(arr);
+const test =Array.from(set);
+test.push("tometo")
+console.log(test);
+
+
+const arr1=["mango", "banana"];
+const set1=new Set(arr1);
+const test1=Array.from(set1);
+test1.push("apple");
+console.log(test1);
