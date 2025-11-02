afad309c478c4ebf79a81b9140e5f5870734e6e3
Mokarama <mokaramaaktershanta@gmail.com>
2025-11-02 18:33:25 +0600

    first commit

---
+++ M01V07.js
@@ -0,0 +1,20 @@
+// for (let i=0; i<=5000; i++){
+//     console.log(i)
+// }
+
+for (let i=0; i<=5000; i++){
+    console.log(i)
+}
+
+for (let i=0; i<=5000; i++){
+    console.log(i)
+}
+
+for (let i=0; i<=5000; i++){
+    console.log(i)
+}
+
+for (let i=0; i<=5000; i++)
+{
+    console.log(i)
+}
