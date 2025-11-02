8879a0c7881619ae96b04d2f810cc9d5604ff185
Mokarama <mokaramaaktershanta@gmail.com>
2025-11-02 23:48:25 +0600

    added set_delete.js file

---
+++ set_delete.js
@@ -0,0 +1,14 @@
+const array=["mango", "banana", "apple"];
+
+const set =new Set(array);
+
+console.log(set.delete("apple"));
+console.log(set)
+
+
+
+const arr=["mango", "banana", "apple"];
+
+const set1=new Set(arr);
+console.log(set1.delete("apple"));
+console.log(set1)
